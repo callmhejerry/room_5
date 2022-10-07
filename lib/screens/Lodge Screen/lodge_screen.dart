4cf4112645047642fe931289960a79f57faedea6
Chinedu Jeremiah <chinedujeremiah2002@gmail.com>
2022-10-07 23:41:03 +0100

    Added lodge_screen.dart file

--- lib/screens/Lodge Screen/lodge_screen.dart
+++ lib/screens/Lodge Screen/lodge_screen.dart
@@ -10,198 +10,234 @@ class LodgeScreen extends StatelessWidget {
     PageController pageController = PageController();
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  SizedBox(
-                    height: 250.h,
-                    width: double.infinity,
-                    child: PageView(
-                      controller: pageController,
-                      children: [
-                        DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(.30),
-                          ),
-                          child: Image.asset(
-                            "assets/images/room1.jpg",
-                            fit: BoxFit.cover,
-                            width: double.infinity,
-                          ),
-                        ),
-                        DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(.30),
-                          ),
-                          child: Image.asset(
-                            "assets/images/yahoo.jpg",
-                            fit: BoxFit.cover,
-                            width: double.infinity,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Positioned.fill(
-                  //   child: DecoratedBox(
-                  //     decoration: BoxDecoration(
-                  //       color: Colors.black.withOpacity(.30),
-                  //     ),
-                  //   ),
-                  // ),
-                  Positioned(
-                    bottom: 12.h,
-                    child: SmoothPageIndicator(
-                      controller: pageController,
-                      count: 2,
-                      effect: SlideEffect(
-                        dotColor: Theme.of(context).colorScheme.surface,
-                        dotHeight: 10,
-                        dotWidth: 10,
-                        activeDotColor: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 16.h,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.arrow_back_ios,
-                              color: Colors.white,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.favorite_border_outlined,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 16.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Text(
-                  "Next Level Lodge",
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                ),
-              ),
-              SizedBox(
-                height: 8.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Row(
-                  children: [
-                    const Text("Next level Junction, awka."),
-                    SizedBox(
-                      width: 10.w,
-                    ),
-                    Icon(
-                      Icons.place,
-                      color: Theme.of(context).colorScheme.secondary,
-                      size: 19.r,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 16.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Text(
-                  "Details",
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                ),
-              ),
-              SizedBox(
-                height: 12.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Text(
-                  "Featuring an outdoor parking space.Next level lodge is located in next level awka, so there is proximity to school.Free wifi is available, there is steady power supply as we have our own transformer.The rooms are spacious and each has a wardrobe where you can keep things",
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: Theme.of(context).colorScheme.onSurface,
-                      ),
-                ),
-              ),
-              SizedBox(
-                height: 16.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Text(
-                  "Contact",
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                ),
-              ),
-              SizedBox(
-                height: 12.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16.w,
-                ),
-                child: SizedBox(
-                  child: Column(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  height: 250.h,
+                  width: double.infinity,
+                  child: PageView(
+                    controller: pageController,
                     children: [
-                      ContactWidget(
-                        contact: "Next level junction, awka, anambra.",
-                        icon: Icon(
-                          Icons.place,
-                          size: 19.r,
-                          color: Theme.of(context).colorScheme.secondary,
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(.30),
+                        ),
+                        child: Image.asset(
+                          "assets/images/room1.jpg",
+                          fit: BoxFit.cover,
+                          width: double.infinity,
                         ),
                       ),
-                      ContactWidget(
-                        contact: "Nextlevellodge@gmail.com",
-                        icon: Icon(
-                          Icons.email,
-                          size: 19.r,
-                          color: Theme.of(context).colorScheme.secondary,
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(.30),
+                        ),
+                        child: Image.asset(
+                          "assets/images/yahoo.jpg",
+                          fit: BoxFit.cover,
+                          width: double.infinity,
                         ),
                       ),
-                      ContactWidget(
-                        contact: "09032435493",
-                        icon: Icon(
-                          Icons.phone,
-                          size: 19.r,
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      )
                     ],
                   ),
                 ),
-              )
-            ],
-          ),
+                // Positioned.fill(
+                //   child: DecoratedBox(
+                //     decoration: BoxDecoration(
+                //       color: Colors.black.withOpacity(.30),
+                //     ),
+                //   ),
+                // ),
+                Positioned(
+                  bottom: 12.h,
+                  child: SmoothPageIndicator(
+                    controller: pageController,
+                    count: 2,
+                    effect: SlideEffect(
+                      dotColor: Theme.of(context).colorScheme.surface,
+                      dotHeight: 10,
+                      dotWidth: 10,
+                      activeDotColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 16.h,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_back_ios,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.bookmark_add_outlined,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Text(
+                "Next Level Lodge",
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Row(
+                children: [
+                  const Text("Next level Junction, awka."),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  Icon(
+                    Icons.place,
+                    color: Theme.of(context).colorScheme.secondary,
+                    size: 19.r,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Text(
+                "Details",
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+            ),
+            SizedBox(
+              height: 12.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Text(
+                "Featuring an outdoor parking space.Next level lodge is located in next level awka, so there is proximity to school.Free wifi is available, there is steady power supply as we have our own transformer.The rooms are spacious and each has a wardrobe where you can keep things",
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Text(
+                "Contact",
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+              ),
+            ),
+            SizedBox(
+              height: 12.h,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16.w,
+              ),
+              child: SizedBox(
+                child: Column(
+                  children: [
+                    ContactWidget(
+                      contact: "Next level junction, awka, anambra.",
+                      icon: Icon(
+                        Icons.place,
+                        size: 19.r,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    ContactWidget(
+                      contact: "Nextlevellodge@gmail.com",
+                      icon: Icon(
+                        Icons.email,
+                        size: 19.r,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    ContactWidget(
+                      contact: "09032435493",
+                      icon: Icon(
+                        Icons.phone,
+                        size: 19.r,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "₦150,000",
+                          style:
+                              Theme.of(context).textTheme.titleLarge!.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                        ),
+                        TextSpan(
+                          text: "/year",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.onSurface,
+                              ),
+                        )
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Book Room"),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+          ],
         ),
       ),
     );
